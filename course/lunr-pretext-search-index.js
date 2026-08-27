50645f782d24d8c5090d9063fea26133f9ba4f90
Matt Charnley <112104661+mpcharnley@users.noreply.github.com>
2026-08-27 20:42:59 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,86 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": "  Introduction  This eBook contains all of the course material for our MAT 2010 class this semester. It is NOT a substitute for attending class, participating in the activities, completing the online homework, and reading the textbook. It is meant to be a supplement to all of these, allowing you to go back and review material\/solutions that were confusing to you when you saw them the first time. This is the filled-in version of the book, which contains the details of the class notes (after that class day is complete) and solutions to activities\/homework once they are graded. The blank version of the book, available on Canvas, has the empty version of the notes that you can print to write on during class.    Course Notes  These are the notes that were covered during class time. The hand-written version of the notes that was actually presented is available directly from the course Canvas page. These notes are a typed version that covers the same content in a similar level of detail.    In-Class Activities  Copies of any in-class activities are available here. This is not a substitute for working on the activity in-class, but if you would like a blank copy to reattempt later in the semester, you can find it here.    Handouts  Any helpful handouts or notes sheets will be included here.    Homework  Any hand-in homework that does not come directly from an in-class activity will be available here. The online homework is a separate part of the course, available from our Canvas site.   "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.    "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "wksht-graphing-equations",
+  "level": "1",
+  "url": "wksht-graphing-equations.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Graphing Equations",
+  "body": " Graphing Equations       Dates  Assigned Date:  Due Date:    Purpose  The skill that you will practice in this exercise is graphing equations. Mastering this skill is vital to your development as a student of mathematics. Our textbook shows graphical solutions to most problems alongside the algebraic solution. Having a firm grasp of the basics will enhance a deeper understanding as you progress through the semester and on into Calculus.    Task  Stuff    Resources  Use your notes from the first class, the online eText or contact your instructor when you have questions.    Criteria for Success  Your assignment is due to your instructor by the date listed above. You must submit 1 pdf document as described in the syllabus, through Canvas. Your instructor will be assessing completeness and correctness for each question. This assignment will be worth 20 points.         [2] What does the graph of an equation represent?    The set of all solutions points to an equation.      [9] Consider the equation .     Graph the equation using any two points.       a=0  b=3  f(x)=4 - (2\/3)*x      (0,4)    (3,2)           Graph the equation using intercepts.       a=0  b=6  f(x)=4 - (2\/3)*x      (0,4)    (6,0)           Graph the equation using the slope and -intercept.      Fully explain each method and your solution.    "
+},
+{
+  "id": "wksht-graphing-equations-3",
+  "level": "2",
+  "url": "wksht-graphing-equations.html#wksht-graphing-equations-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  [2] What does the graph of an equation represent?    The set of all solutions points to an equation.   "
+},
+{
+  "id": "wksht-graphing-equations-4",
+  "level": "2",
+  "url": "wksht-graphing-equations.html#wksht-graphing-equations-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  [9] Consider the equation .     Graph the equation using any two points.       a=0  b=3  f(x)=4 - (2\/3)*x      (0,4)    (3,2)           Graph the equation using intercepts.       a=0  b=6  f(x)=4 - (2\/3)*x      (0,4)    (6,0)           Graph the equation using the slope and -intercept.      Fully explain each method and your solution.   "
+},
+{
+  "id": "handouts",
+  "level": "1",
+  "url": "handouts.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Handouts",
+  "body": " Handouts    "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Filled Homework",
+  "body": " Filled Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
